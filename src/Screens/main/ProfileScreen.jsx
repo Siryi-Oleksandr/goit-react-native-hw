6779b041bdcdbf9e2c6201bae3245617f6572421
Siryi-Oleksandr <siryyoleksandr@gmail.com>
2023-04-10 19:47:52 +0300

    have done profile styles

--- src/Screens/main/ProfileScreen.jsx
+++ src/Screens/main/ProfileScreen.jsx
@@ -11,6 +11,7 @@ import {
   TouchableWithoutFeedback,
   KeyboardAvoidingView,
   ImageBackground,
+  ScrollView,
 } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
 import { pallete } from "../../helpers/variables";
@@ -34,69 +35,129 @@ export function ProfileScreen({ navigation }) {
 
   return (
     <View style={styles.container}>
-      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
-        <View style={styles.form}>
-          <View style={styles.imgWrapper}>
-            {!showAvatar ? (
-              <TouchableOpacity
-                activeOpacity={0.8}
-                style={{
-                  ...styles.btnAddAvatar,
-                  borderColor: pallete.accent,
-                }}
-                onPress={toggleShowAvatar}
-              >
-                <Icon name="plus" size={15} color={pallete.accent} />
-              </TouchableOpacity>
-            ) : (
-              <>
-                <Image
-                  style={styles.avatar}
-                  source={require("../../images/avatar.jpg")}
-                  alt="user avatar"
-                />
+      <ScrollView>
+        <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+          <View style={styles.form}>
+            <View style={styles.imgWrapper}>
+              {!showAvatar ? (
                 <TouchableOpacity
                   activeOpacity={0.8}
                   style={{
                     ...styles.btnAddAvatar,
-                    borderColor: pallete.grey,
+                    borderColor: pallete.accent,
                   }}
                   onPress={toggleShowAvatar}
                 >
-                  <Icon name="times" size={15} color={pallete.grey} />
+                  <Icon name="plus" size={15} color={pallete.accent} />
                 </TouchableOpacity>
-              </>
-            )}
+              ) : (
+                <>
+                  <Image
+                    style={styles.avatar}
+                    source={require("../../images/avatar.jpg")}
+                    alt="user avatar"
+                  />
+                  <TouchableOpacity
+                    activeOpacity={0.8}
+                    style={{
+                      ...styles.btnAddAvatar,
+                      borderColor: pallete.grey,
+                    }}
+                    onPress={toggleShowAvatar}
+                  >
+                    <Icon name="times" size={15} color={pallete.grey} />
+                  </TouchableOpacity>
+                </>
+              )}
+            </View>
+
+            <TouchableOpacity
+              activeOpacity={0.7}
+              style={styles.logOutBtn}
+              onPress={() => alert("Log out from your acount NEW")}
+            >
+              <Icon name="sign-out" size={24} color={pallete.grey} />
+            </TouchableOpacity>
+
+            <Text
+              style={{
+                ...styles.title,
+                marginBottom: orientation === "portrait" ? 33 : 10,
+              }}
+            >
+              Natali Romanova
+            </Text>
+
+            <View style={styles.postWrapper}>
+              <Image
+                style={styles.img}
+                source={require("../../images/nature-1.jpg")}
+                alt="user post picture"
+              />
+              <Text style={styles.postTitle}>Forest</Text>
+              <View style={styles.postInfo}>
+                <View style={styles.postValuesWrapper}>
+                  <View style={styles.postValues}>
+                    <Icon name="comment" size={18} color={pallete.accent} />
+                    <Text style={styles.postValuesText}>8</Text>
+                  </View>
+
+                  <View style={{ ...styles.postValues, marginLeft: 24 }}>
+                    <Icon name="thumbs-o-up" size={18} color={pallete.accent} />
+                    <Text style={styles.postValuesText}>200</Text>
+                  </View>
+                </View>
+
+                <View style={styles.postValues}>
+                  <Icon name="map-marker" size={20} color={pallete.grey} />
+                  <Text
+                    style={{
+                      ...styles.postValuesText,
+                      textDecorationLine: "underline",
+                    }}
+                  >
+                    Ukraine
+                  </Text>
+                </View>
+              </View>
+            </View>
+
+            <View style={styles.postWrapper}>
+              <Image
+                style={styles.img}
+                source={require("../../images/nature-2.jpg")}
+                alt="user post picture"
+              />
+              <Text style={styles.postTitle}>Forest</Text>
+              <View style={styles.postInfo}>
+                <View style={styles.postValuesWrapper}>
+                  <View style={styles.postValues}>
+                    <Icon name="comment" size={18} color={pallete.accent} />
+                    <Text style={styles.postValuesText}>16</Text>
+                  </View>
+
+                  <View style={{ ...styles.postValues, marginLeft: 24 }}>
+                    <Icon name="thumbs-o-up" size={18} color={pallete.accent} />
+                    <Text style={styles.postValuesText}>53</Text>
+                  </View>
+                </View>
+
+                <View style={styles.postValues}>
+                  <Icon name="map-marker" size={20} color={pallete.grey} />
+                  <Text
+                    style={{
+                      ...styles.postValuesText,
+                      textDecorationLine: "underline",
+                    }}
+                  >
+                    Ukraine
+                  </Text>
+                </View>
+              </View>
+            </View>
           </View>
-
-          <TouchableOpacity
-            activeOpacity={0.7}
-            style={styles.logOutBtn}
-            onPress={() => alert("Log out from your acount NEW")}
-          >
-            <Icon name="sign-out" size={24} color={pallete.grey} />
-          </TouchableOpacity>
-
-          <Text
-            style={{
-              ...styles.title,
-              marginBottom: orientation === "portrait" ? 33 : 10,
-            }}
-          >
-            Natali Romanova
-          </Text>
-
-          <View style={styles.postWrapper}>
-            <Image
-              style={styles.img}
-              source={require("../../images/nature-1.jpg")}
-              alt="user post picture"
-            />
-            <Text style={styles.postTitle}>Forest</Text>
-            <View style={styles.postInfo}></View>
-          </View>
-        </View>
-      </ImageBackground>
+        </ImageBackground>
+      </ScrollView>
     </View>
   );
 }
@@ -117,6 +178,7 @@ const styles = StyleSheet.create({
   },
   form: {
     position: "relative",
+    marginTop: 100,
     paddingLeft: 40,
     paddingRight: 40,
     borderTopLeftRadius: 25,
@@ -141,7 +203,9 @@ const styles = StyleSheet.create({
     textAlign: "center",
     color: pallete.black,
   },
-  postWrapper: {},
+  postWrapper: {
+    marginBottom: 32,
+  },
   postTitle: {
     marginTop: 8,
     fontFamily: "Roboto-Medium",
@@ -150,7 +214,25 @@ const styles = StyleSheet.create({
     lineHeight: 19,
     color: pallete.black,
   },
-  postInfo: {},
+  postInfo: {
+    marginTop: 8,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  postValues: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  postValuesText: {
+    marginLeft: 8,
+    fontSize: 16,
+    lineHeight: 19,
+    color: pallete.black,
+  },
+  postValuesWrapper: {
+    flexDirection: "row",
+  },
 
   imgWrapper: {
     position: "absolute",
