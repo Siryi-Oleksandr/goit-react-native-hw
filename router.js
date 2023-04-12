dfbd797d611ff511355fc9a42e24a74eb27e935d
Siryi-Oleksandr <siryyoleksandr@gmail.com>
2023-04-12 12:01:37 +0300

    change logic routers

--- router.js
+++ router.js
@@ -1,7 +1,8 @@
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
-import { StyleSheet } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { StyleSheet, TouchableOpacity } from "react-native";
 import { RegistrationScreen, LoginScreen } from "./src/Screens/auth";
 import {
   CreatePostsScreen,
@@ -14,6 +15,7 @@ import {
 import { pallete } from "./src/helpers/variables";
 
 const AuthStack = createNativeStackNavigator();
+const HomeStack = createNativeStackNavigator();
 const Tab = createMaterialBottomTabNavigator();
 
 export const useRoutes = (isAuth) => {
@@ -35,53 +37,89 @@ export const useRoutes = (isAuth) => {
     );
   } else {
     return (
-      <Tab.Navigator
-        initialRouteName="Home"
-        barStyle={styles.tabBar}
-        activeColor={pallete.accent}
-        labeled={false}
-        shifting={true}
-      >
-        <Tab.Screen
+      <HomeStack.Navigator initialRouteName="Home">
+        <HomeStack.Screen
           name="Home"
           component={Home}
-          labeled={false}
           options={{
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons
-                name="view-grid-outline"
-                color={color}
-                size={24}
-              />
-            ),
+            headerRight: () => LogOut(),
+            headerTitleAlign: "center",
           }}
         />
-        <Tab.Screen
-          name="Create"
-          component={CreatePostsScreen}
-          options={{
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name="plus" color={color} size={24} />
-            ),
-          }}
+
+        <HomeStack.Screen
+          name="Comments"
+          component={CommentsScreen}
+          options={{ headerTitleAlign: "center" }}
         />
-        <Tab.Screen
-          name="Profile"
-          component={ProfileScreen}
-          options={{
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons
-                name="account-outline"
-                color={color}
-                size={24}
-              />
-            ),
-          }}
+        <HomeStack.Screen
+          name="Map"
+          component={MapScreen}
+          options={{ headerTitleAlign: "center" }}
         />
-      </Tab.Navigator>
+      </HomeStack.Navigator>
+
+      // <Tab.Navigator
+      //   initialRouteName="Home"
+      //   barStyle={styles.tabBar}
+      //   activeColor={pallete.accent}
+      //   labeled={false}
+      //   shifting={true}
+      // >
+      //   <Tab.Screen
+      //     name="Home"
+      //     component={Home}
+      //     labeled={false}
+      //     options={{
+      //       tabBarIcon: ({ color }) => (
+      //         <MaterialCommunityIcons
+      //           name="view-grid-outline"
+      //           color={color}
+      //           size={24}
+      //         />
+      //       ),
+      //     }}
+      //   />
+      //   <Tab.Screen
+      //     name="Create"
+      //     component={CreatePostsScreen}
+      //     options={{
+      //       tabBarIcon: ({ color }) => (
+      //         <MaterialCommunityIcons name="plus" color={color} size={24} />
+      //       ),
+      //     }}
+      //   />
+      //   <Tab.Screen
+      //     name="Profile"
+      //     component={ProfileScreen}
+      //     options={{
+      //       tabBarIcon: ({ color }) => (
+      //         <MaterialCommunityIcons
+      //           name="account-outline"
+      //           color={color}
+      //           size={24}
+      //         />
+      //       ),
+      //     }}
+      //   />
+      // </Tab.Navigator>
     );
   }
 };
+
+function LogOut() {
+  return (
+    <TouchableOpacity
+      activeOpacity={0.7}
+      style={{
+        marginRight: 16,
+      }}
+      onPress={() => alert("Log out from your acount NEW")}
+    >
+      <Icon name="sign-out" size={24} color={pallete.gray} />
+    </TouchableOpacity>
+  );
+}
 
 const styles = StyleSheet.create({
   tabBar: {
--- src/Screens/main/Home.jsx
+++ src/Screens/main/Home.jsx
@@ -1,45 +1,93 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import Icon from "react-native-vector-icons/FontAwesome";
 import { CommentsScreen } from "./CommentsScreen";
 import { MapScreen } from "./MapScreen";
+import { ProfileScreen } from "./ProfileScreen";
 import { PostsScreen } from "./PostsScreen";
-import { TouchableOpacity } from "react-native";
+import { TouchableOpacity, StyleSheet } from "react-native";
 import { pallete } from "../../helpers/variables";
 import { CreatePostsScreen } from "./CreatePostsScreen";
 
 const HomeStack = createNativeStackNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
 export const Home = () => {
   return (
-    <HomeStack.Navigator initialRouteName="Posts">
-      <HomeStack.Screen
+    <Tab.Navigator
+      initialRouteName="Posts"
+      barStyle={styles.tabBar}
+      activeColor={pallete.accent}
+      labeled={false}
+      shifting={true}
+    >
+      <Tab.Screen
         name="Posts"
         component={PostsScreen}
+        labeled={false}
         options={{
-          headerRight: () => LogOut(),
-          headerTitleAlign: "center",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="view-grid-outline"
+              color={color}
+              size={24}
+            />
+          ),
         }}
       />
-      <HomeStack.Screen
-        name="Comments"
-        component={CommentsScreen}
-        options={{ headerTitleAlign: "center" }}
-      />
-      <HomeStack.Screen
-        name="Map"
-        component={MapScreen}
-        options={{ headerTitleAlign: "center" }}
-      />
-      {/* <HomeStack.Screen
+      <Tab.Screen
         name="Create"
         component={CreatePostsScreen}
         options={{
-          headerTitleAlign: "center",
-          headerTitle: "Create publication",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="plus" color={color} size={24} />
+          ),
         }}
-      /> */}
-    </HomeStack.Navigator>
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="account-outline"
+              color={color}
+              size={24}
+            />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+    // <HomeStack.Navigator initialRouteName="Posts">
+    //   <HomeStack.Screen
+    //     name="Posts"
+    //     component={PostsScreen}
+    //     options={{
+    //       headerRight: () => LogOut(),
+    //       headerTitleAlign: "center",
+    //     }}
+    //   />
+    //   <HomeStack.Screen
+    //     name="Comments"
+    //     component={CommentsScreen}
+    //     options={{ headerTitleAlign: "center" }}
+    //   />
+    //   <HomeStack.Screen
+    //     name="Map"
+    //     component={MapScreen}
+    //     options={{ headerTitleAlign: "center" }}
+    //   />
+    //   {/* <HomeStack.Screen
+    //     name="Create"
+    //     component={CreatePostsScreen}
+    //     options={{
+    //       headerTitleAlign: "center",
+    //       headerTitle: "Create publication",
+    //     }}
+    //   /> */}
+    // </HomeStack.Navigator>
   );
 };
 
@@ -56,3 +104,12 @@ function LogOut() {
     </TouchableOpacity>
   );
 }
+
+const styles = StyleSheet.create({
+  tabBar: {
+    backgroundColor: pallete.white,
+    // paddingBottom: 16,
+    borderTopWidth: 1,
+    borderTopColor: pallete.gray,
+  },
+});
