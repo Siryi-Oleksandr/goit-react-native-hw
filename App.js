d3f7b09c241d7a1c0d01c0b08b64777618c5c7c9
Siryi-Oleksandr <siryyoleksandr@gmail.com>
2023-04-20 09:51:02 +0300

    move loaded fonts to App

--- App.js
+++ App.js
@@ -1,14 +1,55 @@
 import React, { useCallback, useEffect, useState } from "react";
-import { Provider, useDispatch, useSelector } from "react-redux";
+import { Provider } from "react-redux";
 import { store } from "./src/redux/store";
 import { Main } from "./src/components/Main";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import {
+  Keyboard,
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
+
+SplashScreen.preventAutoHideAsync();
 
 // ! Main logic
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Roboto-Regular": require("./src/fonts/Roboto-Regular.ttf"),
+    "Roboto-Medium": require("./src/fonts/Roboto-Medium.ttf"),
+    "Roboto-Bold": require("./src/fonts/Roboto-Bold.ttf"),
+    Lora: require("./src/fonts/Lora-VariableFont.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return <Text>Haven't loaded fonts</Text>;
+  }
+
   return (
-    <Provider store={store}>
-      <Main />
-    </Provider>
+    <TouchableWithoutFeedback
+      onLayout={onLayoutRootView}
+      onPress={Keyboard.dismiss}
+    >
+      <View style={styles.container}>
+        <Provider store={store}>
+          <Main />
+        </Provider>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,33 +1,15 @@
 import React, { useCallback, useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  Dimensions,
-  Keyboard,
-  StyleSheet,
-  Text,
-  TouchableWithoutFeedback,
-  View,
-} from "react-native";
+import { Dimensions } from "react-native";
 import { useRoutes } from "../../router";
-import { useFonts } from "expo-font";
-import * as SplashScreen from "expo-splash-screen";
 import { NavigationContainer } from "@react-navigation/native";
-import { auth } from "../../src/firebase/config";
-import { authSlice } from "../../src/redux/auth/authSlice";
 import { authStateChangeUser } from "../redux/auth/authOperations";
-
-SplashScreen.preventAutoHideAsync();
 
 export function Main() {
   const selectIsAuth = useSelector((state) => state.auth.isAuth);
   const router = useRoutes(selectIsAuth);
   const [orientation, setOrientation] = useState("portrait");
-  const [fontsLoaded] = useFonts({
-    "Roboto-Regular": require("../../src/fonts/Roboto-Regular.ttf"),
-    "Roboto-Medium": require("../../src/fonts/Roboto-Medium.ttf"),
-    "Roboto-Bold": require("../../src/fonts/Roboto-Bold.ttf"),
-    Lora: require("../../src/fonts/Lora-VariableFont.ttf"),
-  });
+
   const dispatch = useDispatch();
 
   // *** listen change User
@@ -51,32 +33,5 @@ export function Main() {
     return () => subscription?.remove();
   }, [getOrientation]);
 
-  const onLayoutRootView = useCallback(async () => {
-    if (fontsLoaded) {
-      await SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded]);
-
-  if (!fontsLoaded) {
-    return <Text>Haven't loaded fonts</Text>;
-  }
-
-  return (
-    <>
-      <TouchableWithoutFeedback
-        onLayout={onLayoutRootView}
-        onPress={Keyboard.dismiss}
-      >
-        <View style={styles.container}>
-          <NavigationContainer>{router}</NavigationContainer>
-        </View>
-      </TouchableWithoutFeedback>
-    </>
-  );
+  return <NavigationContainer>{router}</NavigationContainer>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
